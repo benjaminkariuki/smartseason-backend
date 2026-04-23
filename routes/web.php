5a9d9b22773fbf8ea715169838d4062486c9dcd5
benjaminkariuki <benhaminleyian6@gmail.com>
2026-04-24 01:56:18 +0300

    remove seeders code

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/run-seeders', function () {
-    try {
-        Artisan::call('migrate:fresh', [
-            '--force' => true,
-            '--seed' => true
-        ]);
-        return 'Database Migrated and Seeded Successfully!';
-    } catch (\Exception $e) {
-        return 'Error: ' . $e->getMessage();
-    }
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
