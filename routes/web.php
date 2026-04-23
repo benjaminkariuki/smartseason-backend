bd5f6d9d9e1844896d43446f1902f203e24d24af
benjaminkariuki <benhaminleyian6@gmail.com>
2026-04-24 01:37:10 +0300

    temp: add route to run seeders

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/run-seeders', function () {
+    try {
+        Artisan::call('migrate:fresh', [
+            '--force' => true,
+            '--seed' => true
+        ]);
+        return 'Database Migrated and Seeded Successfully!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
 });
